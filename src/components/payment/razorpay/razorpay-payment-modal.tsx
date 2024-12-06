26cb4f9b3069a270093e2fe02c20dea345562bf0
VinayKumar018 <kumarvinay15381@gmail.com>
2024-12-06 12:39:32 +0530

    updated one more razorpay

--- src/components/payment/razorpay/razorpay-payment-modal.tsx
+++ src/components/payment/razorpay/razorpay-payment-modal.tsx
@@ -87,81 +87,81 @@
 
 
 
-import { useCallback, useEffect } from 'react';
-import useRazorpay from '@lib/use-razorpay';
-import { formatAddress } from '@lib/format-address';
-import { useTranslation } from 'next-i18next';
-import { useSettings } from '@framework/settings';
-import Spinner from '@components/ui/loaders/spinner/spinner';
-import { useOrderPayment } from '@framework/orders';
+// import { useCallback, useEffect } from 'react';
+// import useRazorpay from '@lib/use-razorpay';
+// import { formatAddress } from '@lib/format-address';
+// import { useTranslation } from 'next-i18next';
+// import { useSettings } from '@framework/settings';
+// import Spinner from '@components/ui/loaders/spinner/spinner';
+// import { useOrderPayment } from '@framework/orders';
 
-interface Props {
-  paymentIntentInfo: PaymentIntentInfo;
-  trackingNumber: string;
-  paymentGateway: PaymentGateway;
-}
+// interface Props {
+//   paymentIntentInfo: PaymentIntentInfo;
+//   trackingNumber: string;
+//   paymentGateway: PaymentGateway;
+// }
 
-const RazorpayPaymentModal: React.FC<Props> = ({
-  trackingNumber,
-  paymentIntentInfo,
-}) => {
-  const { t } = useTranslation();
-  const { loadRazorpayScript, checkScriptLoaded } = useRazorpay();
-  const { data: settings, isLoading: isSettingsLoading } = useSettings();
-  const { createOrderPayment } = useOrderPayment();
+// const RazorpayPaymentModal: React.FC<Props> = ({
+//   trackingNumber,
+//   paymentIntentInfo,
+// }) => {
+//   const { t } = useTranslation();
+//   const { loadRazorpayScript, checkScriptLoaded } = useRazorpay();
+//   const { data: settings, isLoading: isSettingsLoading } = useSettings();
+//   const { createOrderPayment } = useOrderPayment();
 
-  const paymentHandle = useCallback(async () => {
-    if (!checkScriptLoaded()) {
-      await loadRazorpayScript();
-    }
+//   const paymentHandle = useCallback(async () => {
+//     if (!checkScriptLoaded()) {
+//       await loadRazorpayScript();
+//     }
 
-    const options = {
-      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
-      amount: paymentIntentInfo.amount!,
-      currency: paymentIntentInfo.currency!,
-      name: paymentIntentInfo.customer_name!,
-      description: `${t('text-order')}#${trackingNumber}`,
-      image: settings?.options?.logo?.original,
-      order_id: paymentIntentInfo.payment_id!,
-      handler: async () => {
-        await createOrderPayment({
-          tracking_number: trackingNumber,
-          payment_gateway: 'razorpay',
-        });
-        // Refresh the page upon successful payment
-        window.location.reload();
-      },
-      prefill: {
-        name: paymentIntentInfo.customer_name,
-        contact: `+${paymentIntentInfo.customer_contact}`,
-        email: paymentIntentInfo.customer_email,
-      },
-      notes: {
-        address: formatAddress(paymentIntentInfo.billing_address),
-      },
-      modal: {
-        ondismiss: () => console.log('Payment modal closed'),
-      },
-    };
+//     const options = {
+//       key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
+//       amount: paymentIntentInfo.amount!,
+//       currency: paymentIntentInfo.currency!,
+//       name: paymentIntentInfo.customer_name!,
+//       description: `${t('text-order')}#${trackingNumber}`,
+//       image: settings?.options?.logo?.original,
+//       order_id: paymentIntentInfo.payment_id!,
+//       handler: async () => {
+//         await createOrderPayment({
+//           tracking_number: trackingNumber,
+//           payment_gateway: 'razorpay',
+//         });
+//         // Refresh the page upon successful payment
+//         window.location.reload();
+//       },
+//       prefill: {
+//         name: paymentIntentInfo.customer_name,
+//         contact: `+${paymentIntentInfo.customer_contact}`,
+//         email: paymentIntentInfo.customer_email,
+//       },
+//       notes: {
+//         address: formatAddress(paymentIntentInfo.billing_address),
+//       },
+//       modal: {
+//         ondismiss: () => console.log('Payment modal closed'),
+//       },
+//     };
 
-    const razorpay = new (window as any).Razorpay(options);
-    razorpay.open();
-  }, [paymentIntentInfo, settings, trackingNumber]);
+//     const razorpay = new (window as any).Razorpay(options);
+//     razorpay.open();
+//   }, [paymentIntentInfo, settings, trackingNumber]);
 
-  useEffect(() => {
-    if (!isSettingsLoading) {
-      paymentHandle();
-    }
-  }, [isSettingsLoading, paymentHandle]);
+//   useEffect(() => {
+//     if (!isSettingsLoading) {
+//       paymentHandle();
+//     }
+//   }, [isSettingsLoading, paymentHandle]);
 
-  if (isSettingsLoading) {
-    return <Spinner showText={false} />;
-  }
+//   if (isSettingsLoading) {
+//     return <Spinner showText={false} />;
+//   }
 
-  return null;
-};
+//   return null;
+// };
 
-export default RazorpayPaymentModal;
+// export default RazorpayPaymentModal;
 
 
 
@@ -239,3 +239,86 @@ export default RazorpayPaymentModal;
 // };
 
 // export default RazorpayPaymentModal;
+
+
+import { useCallback, useEffect, useState } from 'react';
+import useRazorpay from '@lib/use-razorpay';
+import { formatAddress } from '@lib/format-address';
+import { useTranslation } from 'next-i18next';
+import { useSettings } from '@framework/settings';
+import Spinner from '@components/ui/loaders/spinner/spinner';
+import { useOrderPayment } from '@framework/orders';
+
+interface Props {
+  paymentIntentInfo: PaymentIntentInfo;
+  trackingNumber: string;
+  paymentGateway: PaymentGateway;
+}
+
+const RazorpayPaymentModal: React.FC<Props> = ({
+  trackingNumber,
+  paymentIntentInfo,
+}) => {
+  const { t } = useTranslation();
+  const { loadRazorpayScript, checkScriptLoaded } = useRazorpay();
+  const { data: settings, isLoading: isSettingsLoading } = useSettings();
+  const { createOrderPayment } = useOrderPayment();
+  const [isMounted, setIsMounted] = useState(false);
+
+  const paymentHandle = useCallback(async () => {
+    if (!checkScriptLoaded()) {
+      await loadRazorpayScript();
+    }
+
+    const options = {
+      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
+      amount: paymentIntentInfo.amount!,
+      currency: paymentIntentInfo.currency!,
+      name: paymentIntentInfo.customer_name!,
+      description: `${t('text-order')}#${trackingNumber}`,
+      image: settings?.options?.logo?.original,
+      order_id: paymentIntentInfo.payment_id!,
+      handler: async () => {
+        await createOrderPayment({
+          tracking_number: trackingNumber,
+          payment_gateway: 'razorpay',
+        });
+        // Refresh the page upon successful payment
+        window.location.reload();
+      },
+      prefill: {
+        name: paymentIntentInfo.customer_name,
+        contact: `+${paymentIntentInfo.customer_contact}`,
+        email: paymentIntentInfo.customer_email,
+      },
+      notes: {
+        address: formatAddress(paymentIntentInfo.billing_address),
+      },
+      modal: {
+        ondismiss: () => console.log('Payment modal closed'),
+      },
+    };
+
+    const razorpay = new (window as any).Razorpay(options);
+    razorpay.open();
+  }, [paymentIntentInfo, settings, trackingNumber, createOrderPayment, t]);
+
+  useEffect(() => {
+    if (isMounted && !isSettingsLoading) {
+      paymentHandle();
+    }
+  }, [isMounted, isSettingsLoading, paymentHandle]);
+
+  useEffect(() => {
+    // Set isMounted to true after the initial render
+    setIsMounted(true);
+  }, []);
+
+  if (isSettingsLoading) {
+    return <Spinner showText={false} />;
+  }
+
+  return null;
+};
+
+export default RazorpayPaymentModal;
